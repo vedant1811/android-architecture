1e01a0c33b615bc5c9f56e0d75a1fec1860deaca
vedant <vedant@dunzo.in>
2018-06-14 20:13:28 +0530

    Make `empty title should show error` test pass

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/addedittask/components/AddEditRootView.kt
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/addedittask/components/AddEditRootView.kt
@@ -6,11 +6,22 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
 import com.example.android.architecture.blueprints.todoapp.R
+import kotlinx.android.synthetic.main.add_edit_root_view.view.*
 
 class AddEditRootView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
   init {
     View.inflate(context, R.layout.add_edit_root_view, this)
+
+    done_fab.setOnClickListener{ _ -> doneClicked() }
+  }
+
+  private fun doneClicked() {
+    if (add_task_title.text.isNullOrEmpty()) {
+      Snackbar.make(this, R.string.empty_task_message, Snackbar.LENGTH_LONG)
+          .show()
+      return
+    }
   }
 }
